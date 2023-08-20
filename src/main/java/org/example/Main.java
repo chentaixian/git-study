e8b301817814cf7d460d9b062feac4d68e3eb0f0
chentaixian <66363285+chentaixian@users.noreply.github.com>
2023-08-20 15:26:48 +0800

    pull test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("master push first");
         System.out.println("dev 1");
+        System.out.println("pull test");
     }
 }
