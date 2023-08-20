181175d096b7450d30421ab5e053b0c0b03da448
ctx <1417561589@qq.com>
2023-08-20 15:22:32 +0800

    dev 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("master push first");
+        System.out.println("dev 1");
     }
 }
